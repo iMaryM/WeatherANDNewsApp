b93eb1bb15f9501b4dc167e4e1b6bac9c6f68aca
marym <maria_manzhos@tut.by>
2021-09-12 16:51:42 +0300

    - initial commit

---
+++ WeatherANDNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherANDNewsApp
+//
+//  Created by Мария Манжос on 12.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
