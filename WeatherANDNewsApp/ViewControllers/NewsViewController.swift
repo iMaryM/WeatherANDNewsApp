7769b3a2a159287fc3b64419fcc7409a4f752b27
marym <maria_manzhos@tut.by>
2021-11-12 23:07:34 +0300

    added view News

---
+++ WeatherANDNewsApp/ViewControllers/NewsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsViewController.swift
+//  WeatherANDNewsApp
+//
+//  Created by Мария Манжос on 12.11.21.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
